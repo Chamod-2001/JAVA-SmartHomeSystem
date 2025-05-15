abac5288c7dcc6cc66fa63c4bfdb718a66a5baab
Chamod-2001 <chamodlakshitha013@gmail.com>
2025-05-15 22:58:54 +0530

    Add SmartHomeView class for displaying output

---
+++ src/view/smartHomeView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class smartHomeView {
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayDeviceInfo(String info) {
+        System.out.println(info);
+    }
+
+    public void displaySeparator() {
+        System.out.println();
+    }
+}
